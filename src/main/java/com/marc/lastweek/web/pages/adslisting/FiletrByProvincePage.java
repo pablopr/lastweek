54d2b7b457794e6adc39f7d3d138c83466259037
Pablo Alvarez Yañez <pay@macbook-de-pablo-alvarez-yanez.local>
2009-03-28 17:19:30 +0100

    Added basic search page structure

---
+++ src/main/java/com/marc/lastweek/web/pages/adslisting/FiletrByProvincePage.java
@@ -0,0 +1,22 @@
+/*
+ * FiletrByProvincePage.java
+ * Copyright (c) 2009, Monte Alto Research Center, All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Monte Alto Research Center ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Monte Alto Research Center
+ */
+package com.marc.lastweek.web.pages.adslisting;
+
+import org.apache.wicket.PageParameters;
+
+import com.marc.lastweek.web.pages.BasePage;
+
+public class FiletrByProvincePage extends BasePage {
+
+	public FiletrByProvincePage(PageParameters parameters) {
+		super(parameters);
+	}
+}
---
+++ src/main/java/com/marc/lastweek/web/pages/adslisting/FilterByCategoryProvincePage.java
@@ -0,0 +1,23 @@
+/*
+ * FilterByCategoryProvincePage.java
+ * Copyright (c) 2009, Monte Alto Research Center, All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Monte Alto Research Center ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Monte Alto Research Center
+ */
+package com.marc.lastweek.web.pages.adslisting;
+
+import org.apache.wicket.PageParameters;
+
+import com.marc.lastweek.web.pages.BasePage;
+
+public class FilterByCategoryProvincePage extends BasePage {
+
+	public FilterByCategoryProvincePage(PageParameters parameters) {
+		super(parameters);
+	}
+	
+}
