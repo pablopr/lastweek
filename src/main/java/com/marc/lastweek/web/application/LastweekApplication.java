de5938fd88a1e5aac80ced6f8c9e28ab62b25164
pablo pazos <pablopr@gmail.com>
2009-03-28 15:47:07 +0100

    Added comments to the java files with the right MARC header

--- src/main/java/com/marc/lastweek/web/application/LastweekApplication.java
+++ src/main/java/com/marc/lastweek/web/application/LastweekApplication.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2009, Monte Alto Research Center, All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Monte Alto Research Center ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Monte Alto Research Center
+ */
 package com.marc.lastweek.web.application;
 
 
