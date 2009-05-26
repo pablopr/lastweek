ededb035388c77b8b34cbb1bb6a38054fab4a116
Pablo Alvarez Yañez <pay@macbook-de-pablo-alvarez-yanez.local>
2009-05-26 20:12:42 +0200

    refactoring in the facebook part

---
+++ src/main/java/com/marc/lastweek/web/components/facebook/xfbml/FacebookConnectButton.java
@@ -0,0 +1,79 @@
+/*
+ * @(#)FacebookLoginButton.java
+ *
+ * Copyright (c) 2.009, denodo technologies, S.L. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * denodo technologies, S.L. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with denodo technologies S.L.
+ */
+package com.marc.lastweek.web.components.facebook.xfbml;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+
+public class FacebookConnectButton extends WebMarkupContainer {
+	private static final long serialVersionUID = -7829078749283362022L;
+	
+	public static final String SIZE_SMALL = "small";
+	public static final String SIZE_MEDIUM = "medium";
+	public static final String SIZE_LARGE = "large";
+
+	public static final String LENGTH_SHORT = "short";
+	public static final String LENGTH_LONG = "long";
+	
+    private String size;
+    private String length;
+    private Boolean autologout;
+    
+    public FacebookConnectButton(String id) {
+        this(id, "medium", "long", new Boolean(false));
+    }
+    
+    public FacebookConnectButton(String id, String size, String length) {
+    	this(id, size, length, new Boolean(false));
+    }
+    
+    public FacebookConnectButton(String id, String size, String length, Boolean autologout) {
+        super(id);
+        this.size = size;
+        this.length = length;
+        this.autologout = autologout;
+        this.setOutputMarkupId(true);
+    }
+    
+    @Override
+    protected final void onComponentTag(final ComponentTag tag){
+        checkComponentTag(tag, "login-button");
+        tag.put("size",this.size);
+        tag.put("length",this.length);
+        tag.put("autologoutlink",this.autologout.toString());
+    }
+    
+    public String getSize() {
+		return this.size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getLength() {
+		return this.length;
+	}
+
+	public void setLength(String length) {
+		this.length = length;
+	}
+
+	public Boolean getAutologout() {
+		return this.autologout;
+	}
+
+	public void setAutologout(Boolean autologout) {
+		this.autologout = autologout;
+	}
+}
+
